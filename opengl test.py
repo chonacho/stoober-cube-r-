e34b22437969b1691f8171746357741efd7c7bf5
anotheranoob <29111252+anotheranoob@users.noreply.github.com>
2019-02-22 23:33:59 -0500

    Update opengl test.py

--- opengl test.py
+++ opengl test.py
@@ -1,103 +1,95 @@
-from pyglet.gl import *
-from pyglet.window import key
+import pygame
+from pygame.locals import *
+
 import itertools
 
-WINDOW = 400
-INCREMENT = 5
+from OpenGL.GL import *
+from OpenGL.GLU import *
 
 
-class Window(pyglet.window.Window):
-    # Cube 3D start rotation
-    xRotation = yRotation = 30
-
-    def __init__(self, width, height, cube, title=''):
-        super(Window, self).__init__(width, height, title)
-        glClearColor(0, 0, 0, 1)
-        glEnable(GL_DEPTH_TEST)
-        self.cube = cube
-
-        self.center_position_within_face = [list(map(lambda x: x+0.5, a[::-1])) for a in itertools.product([2, 1, 0], [0, 1, 2])]
-        # the one-liner above is responsible for creating a list representing location of centers of individual stickers
-        # within each face
-        self.faces = [[], [], [], [], [], []]
-        self.horizantal_stickers = list(map(self.generate_corners_sticker, self.center_position_within_face))
-        for i in self.horizantal_stickers:
-            currentList0 = []
-            currentList5 = []
-            for x in i:
-                currentList0.append([x[0], x[1], 3])
-                currentList5.append([x[0], x[1], 0])
-            self.faces[0].append(currentList0)
-            self.faces[5].append(currentList5)
-
-    def draw_piece(self, face, position, color):
-        """
-        Face is a number from 0-5 inclusive, corresponds to sublist of Cube
-        position is a number from 0-8 inclusive, corresponds to element of sublist of Cube
-        color is a number from  0-5 inclusive
-        """
-        glPushMatrix()
-        glRotatef(self.xRotation, 1, 0, 0)
-        glRotatef(self.yRotation, 0, 1, 0)
-        glBegin(GL_QUADS)
-        glColor3ub(*color)
-        for x, y, z in self.faces[face][position]:
-            glVertex3f(x*50, y*50, z*50)
-        glEnd()
-        glPopMatrix()
-
-    def on_draw(self):
-        # Clear the current GL Window
-        self.clear()
-
-        # Push Matrix onto stack
-        glPushMatrix()
-
-        glRotatef(self.xRotation, 1, 0, 0)
-        glRotatef(self.yRotation, 0, 1, 0)
-        self.draw_piece(5, 0, [255, 255, 255])
-        self.draw_piece(5, 5, [255, 0, 255])
-        self.draw_piece(5, 7, [0, 255, 255])
-        # Pop Matrix off stack
-        glPopMatrix()
-
-    def on_resize(self, width, height):
-        # set the Viewport
-        glViewport(0, 0, width, height)
-
-        # using Projection mode
-        glMatrixMode(GL_PROJECTION)
-        glLoadIdentity()
-
-        aspectRatio = width / height
-        gluPerspective(35, aspectRatio, 1, 1000)
-
-        glMatrixMode(GL_MODELVIEW)
-        glLoadIdentity()
-        glTranslatef(0, 0, -400)
-
-    def on_text_motion(self, motion):
-        if motion == key.UP:
-            self.xRotation -= INCREMENT
-        elif motion == key.DOWN:
-            self.xRotation += INCREMENT
-        elif motion == key.LEFT:
-            self.yRotation -= INCREMENT
-        elif motion == key.RIGHT:
-            self.yRotation += INCREMENT
-
-    @staticmethod
-    def generate_corners_sticker(center):
-        """
-        generates list of corner coordinates of a sticker given the center's coordinates
-        generates in clockwise order
-        """
-        coordinates = []
-        for i in [[-0.5, 0.5], [0.5, 0.5], [0.5, -0.5], [-0.5, -0.5]]:
-            coordinates.append([center[0]+i[0], center[1]+i[1]])
-        return coordinates
+def generate_corners_sticker(center):
+    """
+    generates list of corner coordinates of a sticker given the center's coordinates
+    generates in clockwise order
+    """
+    coordinates = []
+    for i in [[-0.5, 0.5], [0.5, 0.5], [0.5, -0.5], [-0.5, -0.5]]:
+        coordinates.append([center[0] + i[0], center[1] + i[1]])
+    return coordinates
 
 
-if __name__ == '__main__':
-    Window(WINDOW, WINDOW, 'Pyglet Colored Cube')
-    pyglet.app.run()
+
+
+def Cube():
+    for i in range(len(tempColors)):
+        draw_piece(5, i, tempColors[i])
+        draw_piece(0, i, tempColors[i])
+
+
+def draw_piece(face, position, color):
+    """
+    Face is a number from 0-5 inclusive, corresponds to sublist of Cube
+    position is a number from 0-8 inclusive, corresponds to element of sublist of Cube
+    color is a number from  0-5 inclusive
+    """
+
+    glBegin(GL_QUADS)
+    glColor3ub(*color)
+    for x, y, z in faces[face][position]:
+        print(x, y, z)
+        glVertex3f(x-1.5, y-1.5, z-1.5)
+    glEnd()
+
+
+def main():
+    global tempColors
+    global faces
+    pygame.init()
+    display = (800,600)
+    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+    glEnable(GL_DEPTH_TEST)
+    tempColors = [[255, 0, 0], [255, 165, 0], [255, 255, 0], [0, 128, 0], [0, 0, 255], [128, 0, 128], [255, 255, 255],
+                  [0, 0, 0], [165, 42, 42]]
+
+    center_position_within_face = [list(map(lambda x: x + 0.5, a[::-1])) for a in
+                                   itertools.product([2, 1, 0], [0, 1, 2])]
+    # the one-liner above is responsible for creating a list representing location of centers of individual stickers
+    # within each face
+    faces = [[], [], [], [], [], []]
+    horizantal_stickers = list(map(generate_corners_sticker, center_position_within_face))
+    for i in horizantal_stickers:
+        currentList0 = []
+        currentList5 = []
+        for x in i:
+            # Remember to flip self.faces[0]
+            currentList0.append([x[0], 3 - x[1], 3])
+            currentList5.append([x[0], x[1], 0])
+        faces[0].append(currentList0)
+        faces[5].append(currentList5)
+
+    coordinates = []
+
+    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
+
+    glTranslatef(0.0,0.0, -5)
+
+    while True:
+        keys = pygame.key.get_pressed()
+        if keys[K_LEFT]:
+            glRotatef(-1, 0, 0, 1)
+        if keys[K_RIGHT]:
+            glRotatef(1, 0, 0, 1)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        Cube()
+        pygame.display.flip()
+        pygame.time.wait(10)
+
+
+main()
+
+
